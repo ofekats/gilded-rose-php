40d53135ebae698935b45d9c5b863604bfe89ada
ofekats <ofekats@gmail.com>
2025-12-30 11:41:14 +0200

    Add basic tests for normal items: quality and sellIn behavior

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -10,11 +10,35 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    public function testQualityDecreasesBy1(): void
     {
-        $items = [new Item('foo', 0, 0)];
+        $items = [new Item('item1', 10, 2)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->assertSame(1, $items[0]->quality);
+    }
+
+    public function testSellInDecreasesBy1(): void
+    {
+        $items = [new Item('item1', 10, 2)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(9, $items[0]->sellIn);
+    }
+
+    public function testQualityDontDecreasesBelow0(): void
+    {
+        $items = [new Item('item1', 10, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(0, $items[0]->quality);
+    }
+    
+    public function testQualityDecreasesTwiceAfterSellInPassed(): void
+    {
+        $items = [new Item('item1', 10, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(0, $items[0]->quality);
     }
 }
