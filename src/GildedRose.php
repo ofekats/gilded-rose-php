bde2d94267e0cf45a0edfbd85305654cc951bae0
ofekats <ofekats@gmail.com>
2025-12-30 12:55:39 +0200

    Add updateAgedBrie  for Aged Brie items

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -27,6 +27,20 @@ final class GildedRose
         }
     }
 
+    private function updateAgedBrie(Item $item): void
+    {
+        if ($item->quality < 50) {
+            $item->quality++;
+        }
+
+        $item->sellIn--;
+
+        if ($item->sellIn < 0 && $item->quality < 50) {
+            $item->quality++;
+        }
+    }
+
+
 
     public function updateQuality(): void
     {
