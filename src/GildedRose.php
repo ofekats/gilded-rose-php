f5d306e0ec7e0d60cfd5e917e88a53b6ea0eb17d
ofekats <ofekats@gmail.com>
2025-12-30 12:59:49 +0200

    Add updateBackstagePass function for Backstage passes items

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -40,6 +40,24 @@ final class GildedRose
         }
     }
 
+    private function updateBackstagePass(Item $item): void
+    {
+        if ($item->quality < 50) {
+            $item->quality++;
+            if ($item->sellIn <= 10 && $item->quality < 50) {
+                $item->quality++;
+            }
+            if ($item->sellIn <= 5 && $item->quality < 50) {
+                $item->quality++;
+            }
+        }
+
+        $item->sellIn--;
+
+        if ($item->sellIn < 0) {
+            $item->quality = 0;
+        }
+    }
 
 
     public function updateQuality(): void
