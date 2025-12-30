7a25ce29d1a7215d786a7316d87e66adad3170da
ofekats <ofekats@gmail.com>
2025-12-30 13:32:35 +0200

    Add tests for Conjured items behavior

---
+++ tests/ConjuredItemTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use GildedRose\GildedRose;
+use GildedRose\Item;
+use PHPUnit\Framework\TestCase;
+
+class ConjuredItemTest extends TestCase
+{
+    public function testQualityDecreasesBy2EachDay(): void
+    {
+        $items = [new Item('Conjured', 10, 2)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(0, $items[0]->quality);
+    }
+
+    public function testQualityNeverBelow0(): void
+    {
+        $items = [new Item('Conjured', 10, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(0, $items[0]->quality);
+    }
+
+    public function testQualityDecreasesBy4WhenSellInIs0(): void
+    {
+        $items = [new Item('Conjured', 0, 5)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(1, $items[0]->quality);
+    }
+
+    public function testSellInDecresesBy1(): void
+    {
+        $items = [new Item('Conjured', 10, 7)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(9, $items[0]->sellIn);
+    }
+
+}
